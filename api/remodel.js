b84d85154e469e3d7f0b89e28c27b04aa96e9d40
WhiteRain97 <106761567+WhiteRain97@users.noreply.github.com>
2025-09-13 11:12:21 -0500

    Change model from 'gpt-5' to 'o3-mini'

--- api/remodel.js
+++ api/remodel.js
@@ -37,7 +37,7 @@ export default async function handler(req, res) {
     ];
 
     const resp = await client.responses.create({
-      model: "gpt-5",
+      model: "o3-mini",
       temperature: 0.5,
       input,
     });
